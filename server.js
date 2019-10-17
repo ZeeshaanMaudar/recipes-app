75aa63038151579c750e8384b85b1f9f10cedff3
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-17 15:38:31 +0200

    add comments to server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 
 require('dotenv').config({ path: 'variables.env' });
 
+// Connects to database
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
+// Initialises application
 const app = express();
 
 // setup the server
