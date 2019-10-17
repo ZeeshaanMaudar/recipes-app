6c572d4d6d48e8d3562a66ed990cda83ea1b0630
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-17 15:18:08 +0200

    create MongoDB Atlas Database and connect to it. Ignore variables.env file

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
 
 const app = express();
 
