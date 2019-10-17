c92e38c509bd4da0c43e3d30e394bc467147e76d
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-17 15:56:19 +0200

    import Schema in backend

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 
 require('dotenv').config({ path: 'variables.env' });
 
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
 // Connects to database
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB connected"))
