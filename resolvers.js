25825a1358478b6aba3e2efe3487f73ae41e4150
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-20 22:36:29 +0200

    reolver for Mutation addRecipe

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,18 @@
 exports.resolvers = {
   Query: {
     getAllRecipes: () => {}
+  },
+  Mutation: {
+    addRecipe: async (root, { name, description, category, instructions, username }, { Recipe }) => {
+      const newRecipe = await new Recipe({
+        name,
+        description,
+        category,
+        instructions,
+        username
+      }).save();
+      
+      return newRecipe;
+    }
   }
 };
