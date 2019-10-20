e2bfabca565d0e7133bd301c9d8f784588860ab6
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-20 21:47:31 +0200

    setup middleware to link to express

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ require('dotenv').config({ path: 'variables.env' });
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
 
+// Adding GraphQl - Express Middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } =require('./resolvers');
+
+// create schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
 // Connects to database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, })
   .then(() => console.log("DB connected"))
@@ -14,6 +27,18 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Initialises application
 const app = express();
+
+// Create GraphiQl Application
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Connect Schemas with GraphQl
+app.use('/graphql', graphqlExpress({
+  schema,
+  context: {
+    Recipe,
+    User
+  }
+}));
 
 // setup the server
 const PORT = process.env.PORT || 4444;
