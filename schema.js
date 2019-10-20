e08221db664f1db763cf17d5ad18354c3b6ecf5f
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-20 21:45:23 +0200

    add graphql schema

---
+++ schema.js
@@ -0,0 +1,21 @@
+exports.typeDefs = `
+
+type Recipe {
+  name: String!
+  category: String!
+  description: String!
+  instructions: String!
+  createDate: String
+  likes: Int
+  username: String
+}
+
+type User {
+  username: String! @unique
+  password: String!
+  email: String!
+  joinDate: String
+  favorite: [Recipe]
+}
+
+`;
