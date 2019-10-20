827783e00b0614827aac6091144d9182a4fb1bbf
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-20 18:31:43 +0200

    require body parser for json requests responses

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config({ path: 'variables.env' });
 
@@ -7,7 +8,7 @@ const Recipe = require('./models/Recipe');
 const User = require('./models/User');
 
 // Connects to database
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, })
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
