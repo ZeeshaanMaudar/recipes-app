c72c52e1183a52866760beb774686f0f4db0df31
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-20 18:16:11 +0200

    remove redundant space

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ const app = express();
 const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
-  console.log(`server listening on PORT: ${PORT} `);
+  console.log(`server listening on PORT: ${PORT}`);
 });
