8cfa040ebfbbd9240ab9d387bad1206ee7479a73
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-20 23:15:28 +0200

    connect indexjs file with ApolloProvider

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4444/graphql'
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+, document.getElementById('root'));
