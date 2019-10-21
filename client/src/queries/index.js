87fad7a1ed9f55d4ac900a5e45e554ce9aa9b131
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-21 12:49:36 +0200

    create query file

---
+++ client/src/queries/index.js
@@ -0,0 +1,14 @@
+import { gql } from 'apollo-boost';
+
+export const GET_ALL_RECIPES = gql`
+  query {
+    getAllRecipes {
+      name
+      description
+      instructions
+      category
+      likes
+      createDate
+    }
+  }
+`;
