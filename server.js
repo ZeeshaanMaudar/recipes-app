25904dfd247de77123478cf88181415bbd9fa2f9
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-21 12:59:14 +0200

    require cors for cross domain request from react app to backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config({ path: 'variables.env' });
 
@@ -27,6 +28,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Initialises application
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 // Create GraphiQl Application
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
