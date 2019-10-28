0c0333fb3b7a69a362ada0f9d152b31757b44bb1
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 22:14:08 +0200

    create Search Component

--- client/src/components/Recipe/Search.js
+++ client/src/components/Recipe/Search.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Search = () => (
-  <div>Search Component</div>
+import { ApolloConsumer } from 'react-apollo';
+
+import { SEARCH_RECIPES } from '../../queries';
+import SearchItem from './SearchItem';
+
+const callSearchRecipes = ({ _id, name, likes }) => (
+  <SearchItem key={_id} {...{ _id, name, likes }} />
 );
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: []
+    }
+  }
+
+  handleChange = ({ searchRecipes }) => {
+    this.setState({ searchResults: searchRecipes });
+  }
+
+  render() {
+    const { searchResults } = this.state;
+    return (
+      <ApolloConsumer>
+        {client => {
+          return (
+            <div className="App">
+              <input
+                type="search"
+                placeholder="Search for Recipes"
+                onChange={async event => {
+                  event.persist();
+                  const searchedData = await client.query({
+                    query: SEARCH_RECIPES,
+                    variables: { searchTerm: event.target.value}
+                  });
+                  const { data } = searchedData;
+                  this.handleChange(data);
+                }}
+              />
+              <ul>
+                {searchResults.map(callSearchRecipes)}
+              </ul>
+            </div>
+          );
+        }}
+      </ApolloConsumer>
+    );
+  }
+};
 
 export default Search;
