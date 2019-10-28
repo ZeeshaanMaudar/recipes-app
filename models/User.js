8283b5c79740b38b5fd926498204662c0ababbd8
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 12:58:07 +0200

    encrypting password for users

--- models/User.js
+++ models/User.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const bcrypt = require('bcrypt');
+
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -25,5 +27,22 @@ const UserSchema = new Schema({
     ref: 'Recipe'
   }
 });
+
+UserSchema.pre('save', function(next) {
+  if(!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if(err) return next(err);
+
+      this.password = hash;
+      next();
+    })
+  })
+})
 
 module.exports = mongoose.model('User', UserSchema);
