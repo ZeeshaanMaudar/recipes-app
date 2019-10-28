9d88275040f2ca8f66f1ce07a2e7347daa27201f
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 14:02:46 +0200

    add token to authorization header and send to backend

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,23 @@ import SignUp from './components/Auth/Signup';
 import SignIn from './components/Auth/Signin';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4444/graphql'
+  uri: 'http://localhost:4444/graphql',
+  fetchOptions: {
+    credentials: 'include' 
+  },
+  request: operation => {
+    const token = localStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        authorization: token
+      }
+    })
+  },
+  onError: ({ networkError }) => {
+    if(networkError) {
+      console.log('Network Error', networkError);
+    }
+  }
 });
 
 const Root = () => (
