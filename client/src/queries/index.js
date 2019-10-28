1e210d9e2927f667d6eff13542e1c06b2fb3dd50
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 19:37:24 +0200

    create query for ADD_RECIPE on the client

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -29,6 +29,21 @@ export const GET_RECIPE = gql`
 
 /* Recipes Mutations */
 
+export const ADD_RECIPE = gql`
+  mutation($name: String!, $description: String!, $category: String!, $instructions: String!, $username: String) {
+    addRecipe(name: $name, description: $description, category: $category, instructions: $instructions, username: $username) {
+      _id
+      name
+      category
+      description
+      instructions
+      createDate
+      likes
+      username
+    }
+  }
+`;
+
 /* User Queries */
 
 export const GET_CURRENT_USER = gql`
