33e3d48631aebcd49a674fcd5d3be36f2e9bfe41
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 13:05:48 +0200

    create SignIn Component

---
+++ client/src/components/Auth/Signin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>SignIn</div>
+    );
+  }
+};
+
+export default SignIn;
