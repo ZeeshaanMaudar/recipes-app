ac6b58933fdf526eb273aff3c325ae2072500d8d
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 18:58:54 +0200

    create AddRecipe Component

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddRecipe = () => (
-  <div>AddRecipe Component</div>
-);
+const initialState = {
+  name: '',
+  category: 'Breakfast',
+  description: '',
+  instructions: '',
+  username: ''
+};
+class AddRecipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...initialState };
+  }
+
+  handleChange = element => {
+    const { name, value } = element.target;
+
+    this.setState({ [name]: value });
+  };
+
+  clearState = () => {
+    this.setState({ ...initialState })
+  };
+
+  render() {
+    const { name, category, description, instructions } = this.state;
+
+    return (
+      <div className="App">
+        <h2 className="App">Add Recipe</h2>
+        <form className="form">
+          <input type="text" name="name" placeholder="Recipe Name" value={name} onChange={this.handleChange} />
+          <select name="category" value={category} onChange={this.handleChange}>
+            <option value="Breakfast">Breakfast</option>
+            <option value="Lunch">Lunch</option>
+            <option value="Dinner">Dinner</option>
+            <option value="Snack">Snack</option>
+          </select>
+          <input type="text" name="description" placeholder="Add description" value={description} onChange={this.handleChange} />
+          <textarea name={instructions} placeholder="Add instructions"></textarea>
+          <button type="submit" className="button-primary">Submit</button>
+        </form>
+      </div>
+    )
+  }
+};
 
 export default AddRecipe;
