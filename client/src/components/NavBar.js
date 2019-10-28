77912e370ef80e647c2f574db70babc0628cbb36
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 16:33:50 +0200

    pass session down to get username

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { NavLink } from 'react-router-dom';
 
 const callNavBarUnAuth = () => (
@@ -18,29 +18,35 @@ const callNavBarUnAuth = () => (
   </ul>
 );
 
-const callNavBarAuth = () => (
-  <ul>
-    <li>
-      <NavLink to="/" exact>Home</NavLink>
-    </li>
-    <li>
-      <NavLink to="/search">Search</NavLink>
-    </li>
-    <li>
-      <NavLink to="/recipe/add">Add Recipe</NavLink>
-    </li>
-    <li>
-      <NavLink to="/profile">Profile</NavLink>
-    </li>
-    <li>
-      <button>Signout</button>
-    </li>
-  </ul>
-);
+const callNavBarAuth = (session) => {
+  const { username } = session.getCurrentUser;
+  return (
+    <Fragment>
+      <ul>
+        <li>
+          <NavLink to="/" exact>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/search">Search</NavLink>
+        </li>
+        <li>
+          <NavLink to="/recipe/add">Add Recipe</NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile">Profile</NavLink>
+        </li>
+        <li>
+          <button>Signout</button>
+        </li>
+      </ul>
+      <h4>Welcome, <strong>{username}</strong></h4>
+    </Fragment>
+  );
+};
 
 const callNavBar = session => {
   if(session && session.getCurrentUser) {
-    return callNavBarAuth();
+    return callNavBarAuth(session);
   }
   
   return callNavBarUnAuth();
