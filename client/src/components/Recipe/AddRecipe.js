7a808147b42b520a927332cab287cb1733b23d56
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 19:38:02 +0200

    query and connect to backend

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import { Mutation } from 'react-apollo';
+import { ADD_RECIPE } from '../../queries';
+import Error from '../Error';
+
 const initialState = {
   name: '',
   category: 'Breakfast',
@@ -29,26 +33,65 @@ class AddRecipe extends Component {
     this.setState({ ...initialState })
   };
 
-  render() {
+  validateForm = () => {
     const { name, category, description, instructions } = this.state;
 
+    const isInvalid = !name || !category || !description || !instructions;
+
+    return isInvalid;
+  };
+
+  handleSubmit = (event, addRecipe) => {
+    event.preventDefault();
+    addRecipe().then(({ data }) => {
+      console.log(888, data)
+    });
+  };
+
+  render() {
+    const { name, category, description, instructions, username } = this.state;
+    
     return (
-      <div className="App">
-        <h2 className="App">Add Recipe</h2>
-        <form className="form">
-          <input type="text" name="name" placeholder="Recipe Name" value={name} onChange={this.handleChange} />
-          <select name="category" value={category} onChange={this.handleChange}>
-            <option value="Breakfast">Breakfast</option>
-            <option value="Lunch">Lunch</option>
-            <option value="Dinner">Dinner</option>
-            <option value="Snack">Snack</option>
-          </select>
-          <input type="text" name="description" placeholder="Add description" value={description} onChange={this.handleChange} />
-          <textarea name="instructions" placeholder="Add instructions" value={instructions} onChange={this.handleChange} />
-          <button type="submit" className="button-primary">Submit</button>
-        </form>
-      </div>
-    )
+      <Mutation mutation={ADD_RECIPE} variables={{ name, category, description, instructions, username }}>
+        {(addRecipe, { data, loading, error }) => {
+          return (
+            <div className="App">
+              <h2 className="App">Add Recipe</h2>
+              <form className="form" onSubmit={event => this.handleSubmit(event, addRecipe)}>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Recipe Name"
+                  value={name}
+                  onChange={this.handleChange}
+                />
+                <select name="category" value={category} onChange={this.handleChange}>
+                  <option value="Breakfast">Breakfast</option>
+                  <option value="Lunch">Lunch</option>
+                  <option value="Dinner">Dinner</option>
+                  <option value="Snack">Snack</option>
+                </select>
+                <input
+                  type="text"
+                  name="description"
+                  placeholder="Add description"
+                  value={description}
+                  onChange={this.handleChange}
+                />
+                <textarea
+                  name="instructions"
+                  placeholder="Add instructions"
+                  value={instructions}
+                  onChange={this.handleChange}
+                />
+                <button type="submit" className="button-primary" disabled={loading || this.validateForm()}>Submit</button>
+                {error && <Error {...{ error }} />}
+              </form>
+            </div>
+          );
+        }}
+      </Mutation>
+    );
   }
 };
 
