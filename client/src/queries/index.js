e343ab9604ef8f1c19970257c77a2d0612fe6d20
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-28 13:40:58 +0200

    implement signin mutation on the client

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -20,6 +20,15 @@ export const GET_ALL_RECIPES = gql`
 /* User Queries */
 
 /* User Mutations */
+
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
 export const SIGNUP_USER = gql`
   mutation($username: String!, $email: String!, $password: String!) {
     signupUser(username: $username, email: $email, password: $password) {
