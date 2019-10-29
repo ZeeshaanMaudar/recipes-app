009e79bb4073344a8898723e00dd40ea974ae0f6
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-29 14:19:25 +0200

    create withAuth HOC to check if user is authenticated

---
+++ client/src/components/withAuth.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Query } from 'react-apollo';
+import { Redirect } from 'react-router-dom';
+
+import { GET_CURRENT_USER } from '../queries';
+
+const withAuth = conditionFunc => Component => props => (
+  <Query query={GET_CURRENT_USER}>
+    {({ data, loading }) => {
+      if(loading) return null;
+
+      if(conditionFunc(data)) {
+        return <Component {...props} />;
+      }
+
+      return <Redirect to="/" />
+    }}
+  </Query>
+);
+
+export default withAuth;
