7d3cc979d77347f599a8c4537d539e05a9f01ba9
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-29 14:05:15 +0200

    add query for getUserRecipe

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -70,6 +70,16 @@ export const GET_CURRENT_USER = gql`
   }
 `;
 
+export const GET_USER_RECIPES = gql`
+  query($username: String!) {
+    getUserRecipes(username: $username) {
+      _id
+      name
+      likes
+    }
+  }
+`;
+
 /* User Mutations */
 
 export const SIGNIN_USER = gql`
